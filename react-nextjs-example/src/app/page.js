8c27cbc264b0e8efab50b60b85151853a420fe92
Kim-WanKyu <116157873+Kim-WanKyu@users.noreply.github.com>
2024-12-24 13:27:19 +0900

    nextjs13

---
+++ react-nextjs-example/src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      Hello, WEB
+      <p><img src="/hello.png" width={150} alt="hello.png" /></p>
+    </>
+  );
+}
